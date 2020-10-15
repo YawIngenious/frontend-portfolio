92fbc6253054c25b46adff83d33c5e09115342fd
yawingenious <56601250+YawIngenious@users.noreply.github.com>
2020-10-15 15:04:39 +0000

    javascript assignment

---
+++ quiz-app/question.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question": "How many realms of reality exists in the universe?",
+	"option1": "One",
+	"option2": "Five",
+	"option3": "Twelve",
+	"option4": "Two",
+	"answer":  "4"
+}, {
+	"question": "What are names of these realms?",
+	"option1": "Heaven and Earth realms",
+	"option2": "Spiritual and Physical realms",
+	"option3": "Celestial and Terrestrial realms",
+	"option4": "Light and darkness realms",
+	"answer": "2"
+}, {
+	"question": "Which of these realms has more influence and power?",
+	"option1": "Heaven realm",
+	"option2": "Physical realm",
+	"option3": "Spiritual realm",
+	"option4": "Earth realm",
+	"answer": "3"
+}, {
+	"question": "Who is more powerful among these?",
+	"option1": "Man",
+	"option2": "Angel Michael",
+	"option3": "Seraphim",
+	"option4": "Lucifer",
+	"answer": "1"
+}, {
+	"question": "Who is more powerful among these?",
+	"option1": "Holy Spirit",
+	"option2": "Jesus Christ",
+	"option3": "God",
+	"option4": "All the above",
+	"answer": "4"
+}, {
+	"question": "What qualifies a man to be a citizen of heaven?",
+	"option1": "Going to Church",
+	"option2": "Being Born again",
+	"option3": "Being baptized in church",
+	"option4": "Being good",
+	"answer": "2"
+}, {
+	"question": "What is the Gospel?",
+	"option1": "A declaration of faith",
+	"option2": "Preaching the goodness",
+	"option3": "Message about the coming, death and resurrection of Jesus Christ",
+	"option4": "A church fellowship",
+	"answer": "3"
+}, {
+	"question": "What is the Great Commission?",
+	"option1": "The charge to preach the gospel worldwide",
+	"option2": "The One World Government",
+	"option3": "Branch of United Nations",
+	"option4": "Money received on work done",
+	"answer": "1"
+}, {
+	"question": "Who is to be most feared by man?",
+	"option1": "Lucifer",
+	"option2": "Demons",
+	"option3": "Armed Robbers",
+	"option4": "God",
+	"answer": "4"
+}, {
+	"question": "What is the greatest trait that God desires all men to have?",
+	"option1": "Consistency",
+	"option2": "Love",
+	"option3": "Kindness",
+	"option4": "Giving",
+	"answer": "2"
+}]
